3728a8161c9b2afb2d7560883730023a7bce5c16
samsatp <sathianpong829@gmail.com>
2023-01-24 08:55:28 +0200

    additional ops + node/layer adstraction

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Learning DeepLearning 1: Neural Network very basic
 ---
+This repository is a place to practice implementing what I learn from Andrej Karpathy lectures provided by his YouTube channel: https://www.youtube.com/@AndrejKarpathy
+
+Specifically, this repository consists of implementations of basic neural network at a low level. It is entirely for educational purposes, not production level applications. I follow Andrej's lecture and implement things he has done based completely on my understanding without copying any code from anywhere (except for some test run scripts).
---
+++ main.py
@@ -0,0 +1,96 @@
+import math
+import numpy as np
+
+class Value:
+    def __init__(self, data: float, label: str = "", previous = []) -> None:
+        self.data =  data
+        self.label = label
+        self.grad = 0.0
+        self._backward = lambda : None
+        self._previous = previous
+
+    def __repr__(self) -> str:
+        return f"Value(data = {self.data})"
+
+    def __add__(self, other):
+        # Forward pass
+        other = Value(data=other) if not isinstance(other, Value) else other
+        output = Value(data = self.data + other.data, previous=[self, other])
+
+        # Backward pass function
+        def _backward():
+            self.grad += output.grad * 1.0
+            other.grad += output.grad * 1.0
+        output._backward = _backward
+        
+        return output
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __mul__(self, other):
+        # Forward pass
+        other = Value(data=other) if not isinstance(other, Value) else other
+        output = Value(data = self.data * other.data, previous=[self, other])
+
+        # Backward pass function
+        def _backward():
+            self.grad += output.grad * other.data
+            other.grad += output.grad * self.data
+        output._backward = _backward
+
+        return output
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**(-1)
+
+    def __pow__(self, n):
+        assert isinstance(n, (int, float))
+        # Forward pass
+        output = Value(data=self.data ** n, previous=[self])
+
+        # Backward pass function
+        def _backward():
+            self.grad += output.grad * ((n) * self.data ** (n-1))
+        output._backward = _backward
+
+        return output
+
+    def __neg__(self):
+        return -1 * self
+
+
+    def tanh(self):
+        # Forward pass
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        output = Value(data = t, previous=[self])
+        
+        # Backward pass function
+        def _backward():
+            self.grad += (1 - t**2) * output.grad
+        output._backward = _backward
+
+        return output
+
+    def backward(self):
+        self.grad = 1.0
+        def BFS(root):
+            output_order = []
+            stack = [root]
+            while len(stack) > 0:
+                top = stack.pop(0)
+                output_order.append(top)
+                if len(top._previous):
+                    stack.extend(top._previous) 
+            return output_order
+
+        output_order = BFS(self)
+        for node in output_order:
+            node._backward()
